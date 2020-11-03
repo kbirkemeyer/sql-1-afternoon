8113a6979faa229ebb0cfd2cb0a4ec447bf25a9c
Katie <adlerk8@gmail.com>
2020-11-03 14:56:51 -0700

    in progress - table-person completed

--- index.sql
+++ index.sql
@@ -29,7 +29,7 @@ ORDER BY height DESC;
 
 -- 4. List all the people in the person table by height from shortest to tallest.
 SELECT * FROM person
-ORDER BY height;
+ORDER BY height ASC;
 
 -- 5. List all the people in the person table by age from oldest to youngest.
 SELECT * FROM person
@@ -48,13 +48,25 @@ SELECT * FROM person
 WHERE age < 20 OR age > 30;
 
 -- 9. List all the people in the person table that are not 27 (Use not equals).
+SELECT * FROM person
+WHERE age != 27;
 
 -- 10. List all the people in the person table where their favorite color is not red.
+SELECT * FROM person
+WHERE favorite_color != 'red';
 
 -- 11. List all the people in the person table where their favorite color is not red and is not blue.
+SELECT * FROM person
+WHERE favorite_color != 'red' AND favorite_color != 'blue';
 
 -- 12. List all the people in the person table where their favorite color is orange or green.
+SELECT * FROM person
+WHERE favorite_color = 'green' OR favorite_color = 'orange';
 
 -- 13. List all the people in the person table where their favorite color is orange, green or blue (use IN).
+SELECT * FROM person
+WHERE favorite_color IN ('orange', 'green', 'blue');
 
 -- 14. List all the people in the person table where their favorite color is yellow or purple (use IN).
+SELECT * FROM person
+WHERE favorite_color IN ('yellow', 'purple');
